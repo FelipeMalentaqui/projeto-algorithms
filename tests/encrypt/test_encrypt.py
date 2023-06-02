62fe9b63695889f92c00f3795c819825ff2ee39a
Felipe <felipemalentaq@hotmail.com>
2023-06-01 21:30:57 -0300

    requisito 2 em andamento

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('ABCDEF', '3')
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(52, 3)
+
+    key_impar = encrypt_message('ABCDEF', 3)
+    assert key_impar == 'CBA_FED'
+
+    key_par = encrypt_message('ABCDEF', 4)
+    assert key_par == 'FE_DCBA'
