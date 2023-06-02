b9460c19b1753432760c7899e49b92fbe4ffcbf4
Felipe <felipemalentaq@hotmail.com>
2023-06-02 14:45:09 -0300

    requisito 2 Ok

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,7 +3,7 @@ import pytest
 
 
 def test_encrypt_message():
-    palavra = 'ABCDEF'
+    palavra = 'ABCABC'
 
     with pytest.raises(TypeError, match='tipo inválido para key'):
         encrypt_message(palavra, '3')
@@ -12,10 +12,10 @@ def test_encrypt_message():
         encrypt_message(52, 3)
 
     key_impar = encrypt_message(palavra, 3)
-    assert key_impar == 'CBA_FED'
+    assert key_impar == 'CBA_CBA'
 
     key_par = encrypt_message(palavra, 4)
-    assert key_par == 'FE_DCBA'
+    assert key_par == 'CB_ACBA'
 
     key_impar_9 = encrypt_message(palavra, 9)
-    assert key_impar_9 == 'FEDCBA'
+    assert key_impar_9 == 'CBACBA'
