1bc23e43cabfe8433542aed556976963ea04ef99
Felipe <felipemalentaq@hotmail.com>
2023-06-02 14:34:05 -0300

    requisito 2 em andamento

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,34 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    print(high_index)
+
+    nome = word[high_index]
+
+    if word == nome:
+        return True
+
+    if high_index == low_index:
+        return False
+
+    else:
+        nome += word[high_index]
+        is_palindrome_recursive(word, low_index, high_index - 1)
+
+
+word = "ANA"
+a = len(word - 1)
+# saída: True
+
+# word = "SOCOS"
+# saída: True
+
+# word = "REVIVER"
+# saída: True
+
+# word = "COXINHA"
+# saída: False
+
+# word = "AGUA"
+# saída: False
+
+print(is_palindrome_recursive(word, 0, a))
+# is_palindrome_recursive((word, 0, len(word - 1)))
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,13 +3,19 @@ import pytest
 
 
 def test_encrypt_message():
+    palavra = 'ABCDEF'
+
     with pytest.raises(TypeError, match='tipo inválido para key'):
-        encrypt_message('ABCDEF', '3')
+        encrypt_message(palavra, '3')
+
     with pytest.raises(TypeError, match='tipo inválido para message'):
         encrypt_message(52, 3)
 
-    key_impar = encrypt_message('ABCDEF', 3)
+    key_impar = encrypt_message(palavra, 3)
     assert key_impar == 'CBA_FED'
 
-    key_par = encrypt_message('ABCDEF', 4)
+    key_par = encrypt_message(palavra, 4)
     assert key_par == 'FE_DCBA'
+
+    key_impar_9 = encrypt_message(palavra, 9)
+    assert key_impar_9 == 'FEDCBA'
